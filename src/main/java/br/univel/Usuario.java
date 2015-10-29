b31891d4c8469d1c639c033363bba961a98ae7b7
WillianCella <willian.cella.wc@gmail.com>
2015-10-29 18:52:44 -0200

    Criação das Classes Cliente, Usuários e ENUM Gênero

---
+++ src/main/java/br/univel/Usuario.java
@@ -0,0 +1,31 @@
+package br.univel;
+
+public class Usuario {
+	private Cliente idCliente;
+	private int id;
+	private String senha;
+
+	public Cliente getIdCliente() {
+		return idCliente;
+	}
+
+	public void setIdCliente(Cliente idCliente) {
+		this.idCliente = idCliente;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getSenha() {
+		return senha;
+	}
+
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
+}
